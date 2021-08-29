d76b794ef035f110ae5f2aa74cf30ff2015414db
Danya <danil51608@gmail.com>
2021-08-29 16:23:11 +0300

    heroku deployment

--- server/index.js
+++ server/index.js
@@ -46,6 +46,9 @@ app.use('/category', categoryRouter)
 
 if(process.env.NODE_ENV === 'production'){
      app.use(express.static('..client/build'))
+     app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+      });
 }
 
 app.listen(PORT, ()=> {
